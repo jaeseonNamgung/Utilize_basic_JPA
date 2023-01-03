096001d0626bd67bf02ebf9344ecfc2cc13a30c4
jaeseonNamgung <sunnamgung8@naver.com>
2023-01-03 17:28:04 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +23,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    public void addStock(int stockQuantity){
+        this.stockQuantity += stockQuantity;
+    }
+    // 삭제
+    public void removeStock(int stockQuantity){
+        int restStock = this.stockQuantity - stockQuantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
 }
