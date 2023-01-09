349f3efb53263a1b1358d7924661bf13f0304c3a
jaeseonNamgung <sunnamgung8@naver.com>
2023-01-09 10:03:25 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,11 +4,13 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +34,7 @@ public class MemberApiController {
             return new CreateMemberResponse(id);
     }
 
+    // 회원 수정
     @PutMapping("/api/v2/member/{id}")
     public updateMemberResponse updateMemberV2
             (@PathVariable("id")Long id, @RequestBody @Valid updateMemberRequest request){
@@ -39,6 +42,27 @@ public class MemberApiController {
         Member member = memberService.findOne(id);
         return new updateMemberResponse(member.getId(), member.getName());
     }
+
+    // 회원 조회
+    // 1. 엔티티를 사용할 경우
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    // 2. DTO를 사용할 경우
+    @GetMapping("/api/v2/members")
+    public MemberListResponse<List<Member>> membersV2(){
+            List<Member> member = memberService.findMembers();
+            return new MemberListResponse<List<Member>>(member);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberListResponse<T>{
+        private T member;
+    }
+    
 
     @Data
     @AllArgsConstructor
